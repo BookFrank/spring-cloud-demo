aa8547e08eb0ca88f582f93c4c391d866acb6ae4
Frank <bookfrank@foxmail.com>
2017-12-13 16:22:36 +0800

    feat (cloud-eureka-server) : Build the single Eureka Server.
    Build a standalone eureka server.
    
    Practice makes prefect.

---
+++ cloud-eureka-server/src/main/java/com/tazine/server/EurekaServer.java
@@ -0,0 +1,20 @@
+package com.tazine.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册、发现中心
+ *
+ * @author Frank
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
