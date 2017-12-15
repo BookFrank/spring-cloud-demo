15a1beda9baa850ae9bc46be48507d4a4fdd8bfb
Frank <bookfrank@foxmail.com>
2017-12-15 23:50:38 +0800

    feat (cloud-feign-service) : Add the feign-service.

---
+++ cloud-feign-service/src/main/java/com/tazine/feign/FeignApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+}
