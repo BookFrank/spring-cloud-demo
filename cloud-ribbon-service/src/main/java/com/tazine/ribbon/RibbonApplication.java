2c056a5d2daf99e552dec4427b76bbe9628769df
Frank <bookfrank@foxmail.com>
2017-12-15 23:45:17 +0800

    feat (cloud-ribbon-service) : Add the service provided by ribbon.
    Ribbon VS Feign
    
    Practice makes perfect.

---
+++ cloud-ribbon-service/src/main/java/com/tazine/ribbon/RibbonApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonApplication.class, args);
+	}
+}
